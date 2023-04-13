730c129c7953e1093db05835782e43a305d2384e
abdulwahabripeseed <abdul.wahab@ripeseed.io>
2023-04-13 10:44:43 +0500

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,24 @@ import { UserModule } from './user/user.module';
 import { User } from './user/user.entity';
 import { Report } from './report/report.entity';
 import { ReportModule } from './report/report.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'dbsqlite',
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          synchronize: true,
+        };
+      },
     }),
     UserModule,
     ReportModule,
